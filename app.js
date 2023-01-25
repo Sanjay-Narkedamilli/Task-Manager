476debf76622f3edd32ed6df531b5d1f92e7d6c6
Sanjay Narkedamilli <78261242+Sanjay-Narkedamilli@users.noreply.github.com>
2023-01-25 21:19:51 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const app = express();
+const tasks=require("./routes/tasks");
+const connectDB=require("./db/connect");
+require('dotenv').config();
+const notFound=require("./middleware/notfound");
+const errorHandlerMiddleware=require("./middleware/error-handler");
+// middleware
+app.use(express.static('./public'));
+app.use(express.json());
+app.use(errorHandlerMiddleware);
+//routes
+app.use('/api/v1/tasks',tasks)
+app.use(notFound)
+const port = process.env.PORT||3000;
+const start=async ()=>{
+    try{
+        await connectDB(process.env.MONGO_URI)
+        app.listen(port,console.log(`Server is listening on port ${port}......`));
+    }
+    catch(err){console.log(err)}
+}
+
+start();
+
